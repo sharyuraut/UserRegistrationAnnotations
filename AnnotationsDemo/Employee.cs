af16e1d9bc059d5b1e32b4f658551605518c6b98
sharyu raut <sharyukraut210298@gmail.com>
2020-10-16 08:39:32 +0530

    [Sharyu] Add. Annotations in user registration problem

--- AnnotationsDemo/Employee.cs
+++ AnnotationsDemo/Employee.cs
@@ -7,15 +7,17 @@ namespace AnnotationsDemo
 {
     class Employee
     {
+        [RegularExpression("^[A-Z][a-z]+$")]
         [Required (ErrorMessage ="Employee {0} is required")]
         [StringLength(100, MinimumLength =3, ErrorMessage ="Name should be minimum 3 characters and maximum 100 characters")]
         [DataType(DataType.Text)]
         public string Name { get; set; }
 
+        [RegularExpression("^[A-Z][a-z]+$")]
         [Range(18, 99, ErrorMessage ="Age should be above 18 and below 99")]
         public int Age { get; set; }
 
-
+        [RegularExpression("^[+][1-9][0-9]+[1-9][0-9]{9}$")]
         [DataType(DataType.PhoneNumber)]
         [Phone]
         public string PhoneNumber { get; set; }
